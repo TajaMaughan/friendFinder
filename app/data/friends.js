fb270906545fd3648a2d13d1eedc776169c84228
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-02-09 22:49:36 -0700

    survey pushing to friend array

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friends = [
+    {
+        name: "Austin",
+        photo: "https://www.sendah.com/988-large_default/daydream-white-teddy-bear.jpg",
+        scores: [
+            "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"
+        ]
+
+    },
+    {
+        name: "John",
+        photo: "https://rukminim1.flixcart.com/image/832/832/jpodaq80/stuffed-toy/z/d/h/4-feet-cute-blue-fur-heart-teddy-bear-120-stuffed-toy-original-imafbu3nruqfhsev.jpeg?q=70",
+        scores: [
+            "1", "1", "2", "2", "3", "3", "4", "4", "5", "5"
+        ]
+    }
+]
+
+module.exports = friends;
+
+
+for (var i = 0; i < friends.length; i++) {
+    //total difference between scores. Math.abs(parseInt(currentUser - currentFriend))
+}
--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 var express = require("express");
-var path = require("path");
+var app = express();
+//sets port
+var PORT = process.env.PORT || 8000;
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//requires routes files
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//let you know server is working
+app.listen(PORT, function() {
+    console.log("App listening at http://localhost:" + PORT);
+  });
