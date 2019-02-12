30c31d070b1e6728b8848e42a77c03849e3295da
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-02-12 13:44:37 -0700

    took out dummy friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,3 @@
-var friends = [
-    {
-        name: "Austin",
-        img: "https://www.sendah.com/988-large_default/daydream-white-teddy-bear.jpg",
-        scores: [
-            "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"
-        ]
-
-    },
-    {
-        name: "John",
-        img: "https://rukminim1.flixcart.com/image/832/832/jpodaq80/stuffed-toy/z/d/h/4-feet-cute-blue-fur-heart-teddy-bear-120-stuffed-toy-original-imafbu3nruqfhsev.jpeg?q=70",
-        scores: [
-            "5", "5", "5", "5", "5", "5", "5", "5", "5", "5"
-        ]
-    }
-]
+var friends = []
 
 module.exports = friends;
