48ed017c3f667318dff495471f5991376d95836e
TajaMaughan <42856210+TajaMaughan@users.noreply.github.com>
2019-02-12 13:30:51 -0700

    modal working, fields validating

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friends = [
     {
         name: "Austin",
-        photo: "https://www.sendah.com/988-large_default/daydream-white-teddy-bear.jpg",
+        img: "https://www.sendah.com/988-large_default/daydream-white-teddy-bear.jpg",
         scores: [
-            "1", "2", "3", "4", "5", "1", "2", "3", "4", "5"
+            "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"
         ]
 
     },
     {
         name: "John",
-        photo: "https://rukminim1.flixcart.com/image/832/832/jpodaq80/stuffed-toy/z/d/h/4-feet-cute-blue-fur-heart-teddy-bear-120-stuffed-toy-original-imafbu3nruqfhsev.jpeg?q=70",
+        img: "https://rukminim1.flixcart.com/image/832/832/jpodaq80/stuffed-toy/z/d/h/4-feet-cute-blue-fur-heart-teddy-bear-120-stuffed-toy-original-imafbu3nruqfhsev.jpeg?q=70",
         scores: [
-            "1", "1", "2", "2", "3", "3", "4", "4", "5", "5"
+            "5", "5", "5", "5", "5", "5", "5", "5", "5", "5"
         ]
     }
 ]
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,17 +6,27 @@ module.exports = function (app) {
     })
 
     app.post("/api/friends", function (req, res) {
+        var friendMatch = {
+            friendName: "",
+            friendImg: "",
+            friendScore: 100
+        }
         var userInput = req.body;
         var userScores = userInput.scores;
-        var totalDifference = 0;
+        var difference = 0;
         for (var i = 0; i < friends.length; i++) {
-            totalDifference = 0;
+            difference = 0;
             for (var j = 0; j < friends[i].scores.length; j++) {
-                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
-                console.log("This is the " + (j + 1 ) + " score for friend " + (i + 1))
-                console.log(totalDifference + " this is the difference so far");
+                difference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
+            }
+            if(difference <= friendMatch.friendScore) {
+                friendMatch.friendName = friends[i].name;
+                friendMatch.friendImg = friends[i].img;
+                friendMatch.friendScore = difference;
             }
         }
+        console.log(friendMatch);
         friends.push(userInput);
+        res.json(friendMatch);
     })
 }
